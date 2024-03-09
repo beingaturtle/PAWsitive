82c8af207dca043a9da93c8e99894a94d6dd0ed7
Jonathan Liu <jwliu0724@hotmail.com>
2024-03-09 14:36:47 -0800

    changed brackets

--- scripts/constants/taskConstants.js
+++ scripts/constants/taskConstants.js
@@ -6,11 +6,11 @@ const PHYSICAL_TASKS = {
 }
 
 // Constants for list of mental task prompts
-const MENTAL_TASKS = (
+const MENTAL_TASKS = [
     "How are you feeling today?",
     "What would you rate your day today?",
     "What are you feeling grateful for today?"
-);
+];
 
 const TaskType = Object.freeze({
     PHYSICAL : PHYSICAL_TASKS,
