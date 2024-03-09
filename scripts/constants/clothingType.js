2146fd539ad0b11fa4ba8e4114b10940ed34b4ac
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-03-09 07:44:23 -0800

    add image reference to each element

---
+++ scripts/constants/clothingType.js
@@ -0,0 +1,11 @@
+const ClothingType = Object.freeze({
+    HAT : "images/hats/trash.webp",
+    SCARF : "images/scarves/trash.webp",
+    GLASSES : "images/glasses/trash.webp",
+    NECKLACE :"images/necklaces/trash.webp",
+    SHIRT : "images/shirts/trash.webp",
+    PANTS : "images/pants/trash.webp",
+    SHOES : "images/shoes/trash.webp",
+    GLOVES : "images/gloves/trash.webp",
+    PET : "images/pets/trash.webp"
+});
