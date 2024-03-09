89ddf36a1dedaf60214150856982794f5190e810
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-03-09 07:43:32 -0800

    remove function call

--- scripts/currency.js
+++ scripts/currency.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-    populateCurrency();
-})
-
 // populate currency
 function populateCurrency() {
     // set currency box to 
+    document.getElementById("currency").innerHTML = localStorage.getItem("currency");
+}
+
+function updateCurrency(newCurrency) {
+    localStorage.setItem("currency", newCurrency);
     document.getElementById("currency").innerHTML = localStorage.getItem("currency");
 }
