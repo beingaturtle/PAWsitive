1ec8d568ab2d0a4a55b7d7e1a095c2255829e00c
Jonathan Liu <jwliu0724@hotmail.com>
2024-03-09 15:15:18 -0800

    separate journal functions from dailies

---
+++ scripts/journal.js
@@ -0,0 +1,24 @@
+function openJournal() {
+    let journal = document.getElementById("journalScreen");
+    journal.showModal();
+}
+
+function closeJournal() {
+    let journal = document.getElementById("journalScreen");
+    saveJournal();
+    journal.close();
+}
+
+function setUpJournalScreen() {
+    let journalScreen = document.getElementById("journalScreen");
+    let closeButton = document.getElementById("closeJournalButton");
+    
+    closeButton.addEventListener("click", () => {
+        closeJournal();
+    });
+}
+
+function saveJournal() {
+    let journalText = document.getElementById("journalEntry").value;
+    localStorage.setItem("journal", journalText);
+}
