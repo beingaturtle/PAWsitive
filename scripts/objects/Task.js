7ad6cf085b2df660a1247862b37ae80562dbae55
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-03-09 05:02:46 -0800

    finish task object

---
+++ scripts/objects/Task.js
@@ -0,0 +1,41 @@
+class Task {
+    
+    // private fields marked with #
+    #taskDescription
+    #taskType
+    #completed
+
+    // public constructor
+    constructor(taskType) {
+        this.#taskType = taskType;
+        this.#completed = false;
+        this.#taskDescription = this.#pickTaskDescription(taskType);
+    }
+
+    // Getters and Setters
+    getTaskDescription() {
+        return this.#taskDescription;
+    }
+
+    getTaskType() {
+        return this.#taskType;
+    }
+
+    getCompletionStatus() {
+        return this.#completed;
+    }
+
+    setCompletionStatus(boolValue) {
+        this.#completed = boolValue;
+    }
+
+    // private pick task description method
+    #pickTaskDescription(taskType) {
+        if (taskType == TaskType.PHYSICAL) {
+            let randomNum = Math.random() * Object.keys(PHYSICAL_TASKS).length;
+            return PHYSICAL_TASKS[Object.keys(PHYSICAL_TASKS)[randomNum]];
+        }
+        let randomNum = Math.random() * MENTAL_TASKS.length;
+        return MENTAL_TASKS[randomNum];
+    }
+}
