ebcdeb3009445b67818d4cb3bc80ae695c8181e7
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-03-09 08:10:53 -0800

    add floor to index calculation

--- scripts/objects/Clothing.js
+++ scripts/objects/Clothing.js
@@ -1,14 +1,12 @@
 class Clothing {
     // private fields marked with #
-    #clothingType; // ClothingType Enum
-    #imageReference; // String
+    #clothingType; // String for image file path (key = clothing type)
     #color; // String (for hex code)
 
     // public constructor for randomization
     constructor() {
         this.#clothingType = this.#pickClothingType();
         this.#color = this.#pickClothingColor();
-        this.#imageReference = this.#pickImageReference(this.#clothingType);
     }
 
     // Getters and Setters
@@ -20,16 +18,8 @@ class Clothing {
         return this.#color;
     }
 
-    getImageReference() {
-        return this.#imageReference;
-    }
-
     setClothingType(clothingType) {
         this.#clothingType = clothingType;
-    }
-
-    setImageReference(imageReference) {
-        this.#imageReference = imageReference;
     }
 
     setClothingColor(color) {
@@ -38,35 +28,12 @@ class Clothing {
 
     // private methods
     #pickClothingType() {
-        let randomNum = Math.random() * Object.keys(ClothingType).length;
-        return PHYSICAL_TASKS[Object.keys(ClothingType)[randomNum]];
-    }
-
-    #pickImageReference(clothingType) {
-        switch(clothingType) {
-            case ClothingType.HAT:
-                return "images/hats/Bunny.gif"
-            case ClothingType.SCARF:
-                return "images/scarves/Bunny.gif"
-            case ClothingType.NECKLACE:
-                return "images/necklaces/Bunny.gif"
-            case ClothingType.SHIRT:
-                return "images/shirts/Bunny.gif"
-            case ClothingType.PANTS:
-                return "images/pants/Bunny.gif"
-            case ClothingType.SHOES:
-                return "images/shoes/Bunny.gif"
-            case ClothingType.PET:
-                return "images/pets/Bunny.gif"
-            case ClothingType.GLOVES:
-                return "images/gloves/Bunny.gif"
-            case ClothingType.GLASSES:
-                return "images/glasses/Bunny.gif"
-        }
+        let randomNum = Math.floor(Math.random() * Object.keys(ClothingType).length);
+        return ClothingType[Object.keys(ClothingType)[randomNum]];
     }
 
     #pickClothingColor() {
-        let randomNum = Math.random() * Object.keys(colors).length;
-        return PHYSICAL_TASKS[Object.keys(ClothingType)[randomNum]];
+        let randomNum = Math.floor(Math.random() * Object.keys(colors).length);
+        return ClothingType[Object.keys(ClothingType)[randomNum]];
     }
 }
