305ab059a2be562dd81663cf49e7ffaacf556012
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-03-09 21:04:56 -0800

    adjust data saving such that json files are saved instead of individual states

--- scripts/dailies.js
+++ scripts/dailies.js
@@ -1,48 +1,100 @@
-function setupDailyScreen() {
-    let dailyScreen = document.getElementById("dailyScreen");
+function initializeDailyScreen(physicalDaily, mentalDaily) {
     let closeButton = document.getElementById("closeButton");
-    let physicalDaily = document.getElementById("physicalDaily");
-    let mentalDaily = document.getElementById("mentalDaily");
+    let dailyScreen = document.getElementById("dailyScreen");
+    let physicalCheckbox = document.getElementById("physicalDaily");
     let completePhysicalButton = document.getElementById("completePhysicalButton");
     let openJournalButton = document.getElementById("openJournalButton");
-    setDailies();
-
+    
     closeButton.addEventListener("click", () => {
         dailyScreen.close();
-    });
+    })
 
     completePhysicalButton.addEventListener("click", () => {
-        physicalDaily.checked = true;
+        physicalDaily.completed = true;
         completePhysicalButton.disabled = true;
-        localStorage.setItem("physicalCompleted", "true");
-        displayIcon();
-    });
+        physicalCheckbox.checked = true;
+        localStorage.setItem("physicalDaily", JSON.stringify(physicalDaily));
+        updateDailyScreen();
+    })
 
     openJournalButton.addEventListener("click", () => {
-        mentalDaily.checked = true;
+        document.getElementById("journalScreen").showModal();
+    })
+
+    setDailyPrompts(physicalDaily, mentalDaily);
+}
+
+function updateDailyScreen() {
+    // read localStorage for daily completion status
+    let physicalDaily = JSON.parse(localStorage.getItem("physicalDaily"));
+    let mentalDaily = JSON.parse(localStorage.getItem("mentalDaily"));
+
+    // update physical daily
+    let physicalCheckbox = document.getElementById("physicalDaily");
+    let completePhysicalButton = document.getElementById("completePhysicalButton");
+    if (physicalDaily.completed) {
+        physicalCheckbox.checked = true;
+        completePhysicalButton.disabled = true;
+    } else {
+        physicalCheckbox.checked = false;
+        completePhysicalButton.disabled = false;
+    }
+    // update mental daily
+    let mentalCheckbox = document.getElementById("mentalDaily");
+    let openJournalButton = document.getElementById("openJournalButton");
+    if (mentalDaily.completed) {
+        mentalCheckbox.checked = true;
         openJournalButton.disabled = true;
-        setUpJournalScreen();
-        openJournal();
-        localStorage.setItem("mentalCompleted", "true");
-        displayIcon();
-    });
+    } else {
+        mentalCheckbox.checked = false;
+        openJournalButton.disabled = false;
+    }
+    displayDailyButtonIcon(physicalDaily, mentalDaily);
 }
 
-function setDailies() {
-    let physicalDaily = randomizePhysicalDailies();
-    let mentalDaily = randomizeMentalDailies();
-    let physicalDailyPrompt = document.getElementById("physicalDailyPrompt");
-    let mentalDailyPrompt = document.getElementById("mentalDailyPrompt");
-    mentalDailyPrompt.innerHTML = mentalDaily.getTaskPrompt();
-    physicalDailyPrompt.innerHTML = physicalDaily.getTaskPrompt();
+function setDailyPrompts(physicalDaily, mentalDaily) {
+    document.getElementById("physicalDailyPrompt").innerHTML = physicalDaily.prompt;
+    document.getElementById("mentalDailyPrompt").innerHTML = mentalDaily.prompt;
 }
 
-function randomizeMentalDailies() {
-    return new MentalTask();
-
+// dataType will either be physicalDaily or mentalDaily
+function saveToLocalStorage(dataType, extractedData) {
+    localStorage.setItem(dataType, JSON.stringify(extractedData));
 }
 
-function randomizePhysicalDailies() {
+function generateMentalDailies() {
+    let randomNum = Math.floor(Math.random() * TaskType.MENTAL.length)
+    return new MentalTask(randomNum);
+}
+
+function generatePhysicalDailies() {
     let randomNum = Math.floor(Math.random() * Object.keys(TaskType.PHYSICAL).length);
     return new PhysicalTask(randomNum);
+}
+
+function extractPhysicalDailyData(physicalDaily) {
+    let extractedData = {
+        workout : physicalDaily.getWorkout(),
+        completed : physicalDaily.getCompleted(),
+        taskMaximum : physicalDaily.getTaskMaximum(),
+        prompt: physicalDaily.getTaskPrompt()
+    }
+    return extractedData;
+}
+
+function extractMentalDailyData(mentalDaily) {
+    let extractedData = {
+        prompt : mentalDaily.getTaskPrompt(),
+        completed : mentalDaily.getCompleted(),
+        promptResponse : mentalDaily.getPromptResponse(),
+        mood : mentalDaily.getMood()
+    }
+    return extractedData;
+}
+
+function generateDailiesJSON() {
+    let physicalDaily = generatePhysicalDailies();
+    let mentalDaily = generateMentalDailies();
+    saveToLocalStorage("physicalDaily", extractPhysicalDailyData(physicalDaily));
+    saveToLocalStorage("mentalDaily", extractMentalDailyData(mentalDaily));
 }
