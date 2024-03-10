9b6d7e08a3d2fb3317002b9947b6841e603aee2c
Caroline Su <caroline77899@gmail.com>
2024-03-09 18:14:52 -0800

    update shop page

--- scripts/constants/clothingType.js
+++ scripts/constants/clothingType.js
@@ -1,8 +1,8 @@
 const ClothingType = Object.freeze({
-    "hair" : "images/hair/",
+    "hair" : "images/hair/get hair.png",
     "shirt" : "images/shirts/get hoodie.png",
     "pants" : "images/pants/get pants.png",
-    "shoes" : "images/shoes/",
+    "shoes" : "images/shoes/get shoes.png",
     "pet" : "images/pets/get dog.png",
     "coin": "images/coin/get 3coins.png"
 });
