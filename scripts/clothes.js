44b81f1f1eaa8e65f4c194a2daf868c0b447b0d9
Ian Chan <ianchan1108@hotmail.com>
2024-03-09 16:57:56 -0800

    Create pop-up functionality for wardrobe

--- scripts/clothes.js
+++ scripts/clothes.js
@@ -1,15 +1,21 @@
-function toggleWardrobe() {
-    let wardrobe = document.getElementById("clothingButton");
-    
-    if (isWardrobeOpen) {
-        wardrobe.style.display = "none";
-        isWardrobeOpen = false;
-    } else {
-        wardrobe.style.display = "block";
-        isWardrobeOpen = true;
+document.addEventListener("DOMContentLoaded", function() {
+    let isWardrobeOpen = false;
+
+    function toggleWardrobe() {
+        event.preventDefault();
+        let wardrobe = document.getElementById("wardrobeScreen");
+        if (isWardrobeOpen) {
+            wardrobe.style.display="none";
+            isWardrobeOpen = false;
+        } else {
+            wardrobe.style.display="block";
+            isWardrobeOpen = true;
+        }
     }
-}
 
-// Click function for wardrobe
-document.getElementById("clothingButton").addEventListener("click", toggleWardrobe);
-
+    // Click function for wardrobe
+    let clothingButton = document.getElementById("clothingButton");
+    if(clothingButton) {
+        clothingButton.addEventListener("click", toggleWardrobe);
+    }
+});
