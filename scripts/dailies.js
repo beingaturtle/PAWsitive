120edc874690b906de7c3696d890a77abbb8ec82
Ian Chan <ianchan1108@hotmail.com>
2024-03-10 01:45:25 -0800

    Debug displayDailyButtonIcon

--- scripts/dailies.js
+++ scripts/dailies.js
@@ -1,131 +1,150 @@
 function initializeDailyScreen(physicalDaily, mentalDaily) {
-    let closeButton = document.getElementById("closeButton");
-    let dailyScreen = document.getElementById("dailyScreen");
-    let physicalCheckbox = document.getElementById("physicalDaily");
-    let completePhysicalButton = document.getElementById("completePhysicalButton");
-    let openJournalButton = document.getElementById("openJournalButton");
-    let skipDailyButton = document.getElementById("skipDailyButton");
+  let closeButton = document.getElementById("closeButton");
+  let dailyScreen = document.getElementById("dailyScreen");
 
-    // add event listener for close button
-    closeButton.addEventListener("click", () => {
-        dailyScreen.close();
-    })
+  let physicalCheckbox = document.getElementById("physicalDaily");
+  let completePhysicalButton = document.getElementById(
+    "completePhysicalButton"
+  );
+  let openJournalButton = document.getElementById("openJournalButton");
+  let skipDailyButton = document.getElementById("skipDailyButton");
 
-    // add event listener for physical daily completion button
-    completePhysicalButton.addEventListener("click", () => {
-        physicalDaily.completed = true;
-        saveToLocalStorage("physicalDaily", physicalDaily);
-        updateDailyScreen();
-    })
+  // add event listener for close button
+  closeButton.addEventListener("click", () => {
+    dailyScreen.close();
+  });
 
-    // add event listener for button to open journal
-    openJournalButton.addEventListener("click", () => {
-        document.getElementById("journalScreen").showModal();
-    })
- 
-    setDailyPrompts(physicalDaily, mentalDaily);
-    
-    // add event listener to skip button
-    skipDailyButton.addEventListener("click", () => {
-        physicalDaily.completed = true;
-        mentalDaily.completed = true;
-        updateBreakToken(fetchBreakTokenCount() - 1);
-        updateDailyScreen();
-        skipDailyButton.disabled = true;
-    })
-    if (fetchBreakTokenCount() == 0 || (physicalDaily.completed && mentalDaily.completed)) {
-        skipDailyButton.disabled = true;
-    }
+  // add event listener for physical daily completion button
+  completePhysicalButton.addEventListener("click", () => {
+    physicalDaily.completed = true;
+    saveToLocalStorage("physicalDaily", physicalDaily);
+    updateDailyScreen();
+  });
+
+  // add event listener for button to open journal
+  openJournalButton.addEventListener("click", () => {
+    document.getElementById("journalScreen").showModal();
+  });
+
+  setDailyPrompts(physicalDaily, mentalDaily);
+
+  // add event listener to skip button
+  skipDailyButton.addEventListener("click", () => {
+    physicalDaily.completed = true;
+    mentalDaily.completed = true;
+    updateBreakToken(fetchBreakTokenCount() - 1);
+    saveToLocalStorage("physicalDaily", physicalDaily);
+    saveToLocalStorage("mentalDaily", mentalDaily);
+    skipDailyButton.disabled = true;
+    updateDailyScreen();
+  });
+  if (
+    fetchBreakTokenCount() == 0 ||
+    (physicalDaily.completed && mentalDaily.completed)
+  ) {
+    skipDailyButton.disabled = true;
+  }
 }
 
 function updateSkipDailyButton(physicalDaily, mentalDaily) {
-    // handle skip daily button logic
-    let breakTokenCount = fetchBreakTokenCount();
+  // handle skip daily button logic
+  let breakTokenCount = fetchBreakTokenCount();
 
-    if (breakTokenCount == 0 || (physicalDaily.completed && mentalDaily.completed)) {
-        skipDailyButton.disabled = true;
-    } else {
-        // re-enabled button after reset
-        skipDailyButton.disabled = false;
-    }
+  if (
+    breakTokenCount == 0 ||
+    (physicalDaily.completed && mentalDaily.completed)
+  ) {
+    skipDailyButton.disabled = true;
+  } else {
+    // re-enabled button after reset
+    skipDailyButton.disabled = false;
+  }
 }
 
 async function updateDailyScreen() {
-    // read localStorage for daily completion status
-    let physicalDaily = JSON.parse(localStorage.getItem("physicalDaily"));
-    let mentalDaily = JSON.parse(localStorage.getItem("mentalDaily"));
-    console.log(physicalDaily);
+  // read localStorage for daily completion status
+  let physicalDaily = JSON.parse(localStorage.getItem("physicalDaily"));
+  let mentalDaily = JSON.parse(localStorage.getItem("mentalDaily"));
+  console.log(physicalDaily);
 
-    // update physical daily
-    let physicalCheckbox = document.getElementById("physicalDaily");
-    let completePhysicalButton = document.getElementById("completePhysicalButton");
-    
-    if (physicalDaily.completed === true) {
-        physicalCheckbox.checked = true;
-        completePhysicalButton.disabled = true;
-    } else {
-        physicalCheckbox.checked = false;
-        completePhysicalButton.disabled = false;
-    }
-    // update mental daily
-    let mentalCheckbox = document.getElementById("mentalDaily");
-    let openJournalButton = document.getElementById("openJournalButton");
-    if (mentalDaily.completed === true) {
-        mentalCheckbox.checked = true;
-        openJournalButton.disabled = true;
-    } else {
-        mentalCheckbox.checked = false;
-        openJournalButton.disabled = false;
-    }
+  // update physical daily
+  let physicalCheckbox = document.getElementById("physicalDaily");
+  let completePhysicalButton = document.getElementById(
+    "completePhysicalButton"
+  );
 
-    setDailyPrompts(physicalDaily, mentalDaily);
-    updateSkipDailyButton(physicalDaily, mentalDaily);
-    displayDailyButtonIcon(physicalDaily, mentalDaily);
+  if (physicalDaily.completed === true) {
+    physicalCheckbox.checked = true;
+    completePhysicalButton.disabled = true;
+  } else {
+    physicalCheckbox.checked = false;
+    completePhysicalButton.disabled = false;
+  }
+  // update mental daily
+  let mentalCheckbox = document.getElementById("mentalDaily");
+  let openJournalButton = document.getElementById("openJournalButton");
+
+  if (mentalDaily.completed === true) {
+    mentalCheckbox.checked = true;
+    openJournalButton.disabled = true;
+  } else {
+    mentalCheckbox.checked = false;
+    openJournalButton.disabled = false;
+  }
+
+  setDailyPrompts(physicalDaily, mentalDaily);
+
+  updateSkipDailyButton(physicalDaily, mentalDaily);
+
+  displayDailyButtonIcon(physicalDaily, mentalDaily);
 }
 
 function setDailyPrompts(physicalDaily, mentalDaily) {
-    document.getElementById("physicalDailyPrompt").innerHTML = physicalDaily.prompt;
-    document.getElementById("mentalDailyPrompt").innerHTML = mentalDaily.prompt;
+  document.getElementById("physicalDailyPrompt").innerHTML =
+    physicalDaily.prompt;
+  document.getElementById("mentalDailyPrompt").innerHTML = mentalDaily.prompt;
 }
 
 // dataType will either be physicalDaily or mentalDaily
 function saveToLocalStorage(dataType, extractedData) {
-    localStorage.setItem(dataType, JSON.stringify(extractedData));
+  localStorage.setItem(dataType, JSON.stringify(extractedData));
 }
 
 function generateMentalDailies() {
-    let randomNum = Math.floor(Math.random() * TaskType.MENTAL.length)
-    return new MentalTask(randomNum);
+  let randomNum = Math.floor(Math.random() * TaskType.MENTAL.length);
+  return new MentalTask(randomNum);
 }
 
 function generatePhysicalDailies() {
-    let randomNum = Math.floor(Math.random() * Object.keys(TaskType.PHYSICAL).length);
-    return new PhysicalTask(randomNum);
+  let randomNum = Math.floor(
+    Math.random() * Object.keys(TaskType.PHYSICAL).length
+  );
+  return new PhysicalTask(randomNum);
 }
 
 function extractPhysicalDailyData(physicalDaily) {
-    let extractedData = {
-        workout : physicalDaily.getWorkout(),
-        completed : physicalDaily.getCompleted(),
-        taskMaximum : physicalDaily.getTaskMaximum(),
-        prompt: physicalDaily.getTaskPrompt()
-    }
-    return extractedData;
+  let extractedData = {
+    workout: physicalDaily.getWorkout(),
+    completed: physicalDaily.getCompleted(),
+    taskMaximum: physicalDaily.getTaskMaximum(),
+    prompt: physicalDaily.getTaskPrompt(),
+  };
+  return extractedData;
 }
 
 function extractMentalDailyData(mentalDaily) {
-    let extractedData = {
-        prompt : mentalDaily.getTaskPrompt(),
-        completed : mentalDaily.getCompleted(),
-        promptResponse : mentalDaily.getPromptResponse(),
-        mood : mentalDaily.getMood()
-    }
-    return extractedData;
+  let extractedData = {
+    prompt: mentalDaily.getTaskPrompt(),
+    completed: mentalDaily.getCompleted(),
+    promptResponse: mentalDaily.getPromptResponse(),
+    mood: mentalDaily.getMood(),
+  };
+  return extractedData;
 }
 
 function generateDailiesJSON() {
-    let physicalDaily = generatePhysicalDailies();
-    let mentalDaily = generateMentalDailies();
-    saveToLocalStorage("physicalDaily", extractPhysicalDailyData(physicalDaily));
-    saveToLocalStorage("mentalDaily", extractMentalDailyData(mentalDaily));
+  let physicalDaily = generatePhysicalDailies();
+  let mentalDaily = generateMentalDailies();
+  saveToLocalStorage("physicalDaily", extractPhysicalDailyData(physicalDaily));
+  saveToLocalStorage("mentalDaily", extractMentalDailyData(mentalDaily));
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,71 +1,79 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let mentalDaily = JSON.parse(localStorage.getItem("mentalDaily"));
-    let physicalDaily = JSON.parse(localStorage.getItem("physicalDaily"));
-    let currency = localStorage.getItem("currency");
-    let breakTokens = localStorage.getItem("breakTokens");
+  let mentalDaily = JSON.parse(localStorage.getItem("mentalDaily"));
+  let physicalDaily = JSON.parse(localStorage.getItem("physicalDaily"));
+  let currency = localStorage.getItem("currency");
+  let breakTokens = localStorage.getItem("breakTokens");
 
-    // in case first time visit where nothing in local storage
-    if (mentalDaily == null || physicalDaily == null || currency == null || breakTokens == null) {
-        generateDailiesJSON();
-        mentalDaily = JSON.parse(localStorage.getItem("mentalDaily"));
-        physicalDaily = JSON.parse(localStorage.getItem("physicalDaily"));
-        localStorage.setItem("currency", 0);
-        localStorage.setItem("breakTokens", 2);
-    }
+  // in case first time visit where nothing in local storage
+  if (
+    mentalDaily == null ||
+    physicalDaily == null ||
+    currency == null ||
+    breakTokens == null
+  ) {
+    generateDailiesJSON();
+    mentalDaily = JSON.parse(localStorage.getItem("mentalDaily"));
+    physicalDaily = JSON.parse(localStorage.getItem("physicalDaily"));
+    localStorage.setItem("currency", 0);
+    localStorage.setItem("breakTokens", 2);
+  }
 
-    displayDailyButtonIcon(physicalDaily, mentalDaily);
-    initializeButtons();
-    initializeDailyScreen(physicalDaily, mentalDaily);
-    updateDailyScreen();
-    initializeJournalScreen(physicalDaily, mentalDaily);
-    populateCurrency();
-    populateBreakToken();
-})
+  displayDailyButtonIcon(physicalDaily, mentalDaily);
+  initializeButtons();
+  initializeDailyScreen(physicalDaily, mentalDaily);
+  updateDailyScreen();
+  initializeJournalScreen(physicalDaily, mentalDaily);
+  populateCurrency();
+  populateBreakToken();
+});
 
 function displayDailyButtonIcon(physicalDaily, mentalDaily) {
-    let checkmarkIcon = document.getElementById("checkIcon");
-    let alertIcon = document.getElementById("alertIcon");
+  let checkmarkIcon = document.getElementById("checkIcon");
+  let alertIcon = document.getElementById("alertIcon");
 
-    if (physicalDaily.completed && mentalDaily.completed) {
-        // show checkmark upon completion
-        checkmarkIcon.style.display = "block";
-        alertIcon.style.display = "none";
-    } else if (!(physicalDaily.completed && mentalDaily.completed)) {
-        // re-enable all buttons upon reset
-        document.getElementById("openJournalButton").disabled = false;
-        document.getElementById("completePhysicalButton").disabled = false;
-        document.getElementById("openJournalButton").disabled = false;
-        document.getElementById("physicalDaily").checked = false;
-        document.getElementById("mentalDaily").checked = false;
+  if (physicalDaily.completed && mentalDaily.completed) {
+    // show checkmark upon completion
+    checkmarkIcon.style.display = "block";
+    alertIcon.style.display = "none";
+  } else if (!(physicalDaily.completed || mentalDaily.completed)) {
+    // re-enable all buttons upon reset
+    document.getElementById("openJournalButton").disabled = false;
+    document.getElementById("completePhysicalButton").disabled = false;
+    document.getElementById("openJournalButton").disabled = false;
+    document.getElementById("physicalDaily").checked = false;
+    document.getElementById("mentalDaily").checked = false;
 
-        // change style back to default
-        alertIcon.style.color = "black";
-        checkmarkIcon.style.display = "none";
-        alertIcon.style.display = "block";
-    } else {
-        // show warning to finish when only one is done
-        checkmarkIcon.style.display = "none";
-        alertIcon.style.display = "block";
-        alertIcon.style.color = "yellow";
-    }
+    // change style back to default
+    alertIcon.style.color = "black";
+    checkmarkIcon.style.display = "none";
+    alertIcon.style.display = "block";
+  } else {
+    // show warning to finish when only one is done
+    checkmarkIcon.style.display = "none";
+    alertIcon.style.display = "block";
+    alertIcon.style.color = "yellow";
+  }
 }
 
 function initializeButtons() {
-    let dailyButton = document.getElementById("dailyBox");
-    dailyButton.addEventListener("click", () => {
-        document.getElementById("dailyScreen").showModal();
-    })
+  let dailyButton = document.getElementById("dailyBox");
+  dailyButton.addEventListener("click", () => {
+    document.getElementById("dailyScreen").showModal();
+  });
 }
 
 // timer for daily reset, runs on 8s for the sake of demo
-setInterval(() => { 
-    generateDailiesJSON();
-    updateDailyScreen();
-    updateSkipDailyButton(localStorage.getItem("physicalDaily"), localStorage.getItem("mentalDaily"));
+setInterval(() => {
+  generateDailiesJSON();
+  updateDailyScreen();
+  updateSkipDailyButton(
+    localStorage.getItem("physicalDaily"),
+    localStorage.getItem("mentalDaily")
+  );
 }, 7000);
 
 // timer for skip token refresh, runs on 30s for the sake of demo
 setInterval(() => {
-    localStorage.setItem("breakTokens", 2);
-    populateBreakToken();
-}, 10000)
+  localStorage.setItem("breakTokens", 2);
+  populateBreakToken();
+}, 10000);
