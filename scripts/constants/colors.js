62ec6fa3be1fde8658d6e0d5bf7d908cb830dedc
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-03-10 00:36:04 -0800

    scrap pull variation idea

--- scripts/constants/colors.js
+++
@@ -1,4 +0,0 @@
-const colors = (
-    "#ffffff",
-    "31c5f4"
-)
--- scripts/objects/Clothing.js
+++ scripts/objects/Clothing.js
@@ -1,21 +1,15 @@
 class Clothing {
     // private fields marked with #
     #clothingType; // String for image file path (key = clothing type)
-    #color; // String (for hex code)
 
     // public constructor for randomization
-    constructor(clothingType, color) {
+    constructor(clothingType) {
         this.#clothingType = clothingType || this.#pickClothingType();
-        this.#color = color || this.#pickClothingColor();
     }
 
     // Getters and Setters
     getClothingType() {
         return this.#clothingType;
-    }
-
-    getClothingColor() {
-        return this.#color;
     }
 
     getClothingName() {
@@ -26,18 +20,9 @@ class Clothing {
         this.#clothingType = clothingType;
     }
 
-    setClothingColor(color) {
-        this.#color = color;
-    }
-
     // private methods
     #pickClothingType() {
         let randomNum = Math.floor(Math.random() * Object.keys(ClothingType).length);
         return ClothingType[Object.keys(ClothingType)[randomNum]];
-    }
-
-    #pickClothingColor() {
-        let randomNum = Math.floor(Math.random() * Object.keys(colors).length);
-        return colors[Object.keys(colors)[randomNum]];
     }
 }
