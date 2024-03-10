a035b99b6023ed5023f1eb502af6f72501fc2d34
Ian Chan <ianchan1108@hotmail.com>
2024-03-10 05:11:48 -0700

    Committed dailies, journal, tasks

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,48 +1,49 @@
 function initializeJournalScreen(physicalDaily, mentalDaily) {
-    // get every button
-    let journalScreen = document.getElementById("journalScreen");
-    let happyButton = document.getElementById("happyButton");
-    let neutralButton = document.getElementById("neutralButton");
-    let sadButton = document.getElementById("sadButton");
-    let journalEntry = document.getElementById("journalEntry");
-    let closeJournalButton = document.getElementById("closeJournalButton");
-    let mood;
+  // get every button
+  let journalScreen = document.getElementById("journalScreen");
+  let happyButton = document.getElementById("happyButton");
+  let neutralButton = document.getElementById("neutralButton");
+  let sadButton = document.getElementById("sadButton");
+  let journalEntry = document.getElementById("journalEntry");
+  let closeJournalButton = document.getElementById("closeJournalButton");
+  let mentalCheckbox = document.getElementById("mentalDaily");
+  let mood;
 
-    // add event listeners to every mood button
-    happyButton.addEventListener("click", () => {
-        // add some styling here
-        mood = "happy";
-    });
-    neutralButton.addEventListener("click", () => {
-        // add some styling here
-        mood = "neutral";
-    });
-    sadButton.addEventListener("click", () => {
-        // add some styling here
-        mood = "sad";
-    });
+  // add event listeners to every mood button
+  happyButton.addEventListener("click", () => {
+    // add some styling here
+    mood = "happy";
+  });
+  neutralButton.addEventListener("click", () => {
+    // add some styling here
+    mood = "neutral";
+  });
+  sadButton.addEventListener("click", () => {
+    // add some styling here
+    mood = "sad";
+  });
 
-    // add event listener for finalize button
-    closeJournalButton.addEventListener("click", () => {
-        if (mood == null) mood = "neutral";
+  // add event listener for finalize button
+  closeJournalButton.addEventListener("click", () => {
+    if (mood == null) mood = "neutral";
 
-        if (!journalEntry.value) {
-            mentalDaily.promptResponse = "You didn't write anything today!";
-        } else {
-            mentalDaily.promptResponse = journalEntry.value;
-        }
-        mentalDaily.mood = mood;
-        mentalDaily.completed = true;
-        saveToLocalStorage("mentalDaily", mentalDaily);
-        journalScreen.close();
-        
-        // disable open journal button
-        let openJournalButton = document.getElementById("openJournalButton");
-        openJournalButton.disabled = true;
-        updateSkipDailyButton(physicalDaily, mentalDaily);
-        updateDailyScreen();
-    });
+    if (!journalEntry.value) {
+      mentalDaily.promptResponse = "You didn't write anything today!";
+    } else {
+      mentalDaily.promptResponse = journalEntry.value;
+    }
+    mentalDaily.mood = mood;
+    mentalDaily.completed = true;
+    mentalCheckbox.checked = true;
+    saveToLocalStorage("mentalDaily", mentalDaily);
+    journalScreen.close();
+
+    // disable open journal button
+    let openJournalButton = document.getElementById("openJournalButton");
+    openJournalButton.disabled = true;
+    updateSkipDailyButton(physicalDaily, mentalDaily);
+    updateDailyScreen();
+  });
 }
-
 
 // add active effect
--- scripts/objects/Task.js
+++ scripts/objects/Task.js
@@ -1,34 +1,33 @@
 class Task {
-    // private fields marked with #
-    #taskType; // TaskType enum
-    #taskPrompt; // String
-    #completed; // boolean
+  // private fields marked with #
+  #taskType; // TaskType enum
+  #taskPrompt; // String
+  #completed; // boolean
 
-    // public constructor
-    constructor(taskType) {
-        this.#taskType = taskType;
-        this.#completed = false;
-    }
+  // public constructor
+  constructor(taskType) {
+    this.#taskType = taskType;
+    this.#completed = false;
+  }
 
-    // Getters and Setters
-    getTaskType() {
-        return this.#taskType;
-    }
+  // Getters and Setters
+  getTaskType() {
+    return this.#taskType;
+  }
 
-    getTaskPrompt() {
-        return this.#taskPrompt;
-    }
+  getTaskPrompt() {
+    return this.#taskPrompt;
+  }
 
-    getCompleted() {
-        return this.#completed;
-    }
+  getCompleted() {
+    return this.#completed;
+  }
 
-    setTaskPrompt(prompt) {
-        this.#taskPrompt = prompt;
-    }
+  setTaskPrompt(prompt) {
+    this.#taskPrompt = prompt;
+  }
 
-    setCompleted(boolean) {
-        this.#taskPrompt = boolean;
-    }
-
+  setCompleted(boolean) {
+    this.#completed = boolean;
+  }
 }
