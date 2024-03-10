9152e8bc5c33b9667fdb5bcb38a93558f2fa6154
Ian Chan <ianchan1108@hotmail.com>
2024-03-09 17:53:14 -0800

    Update pet to pets

--- scripts/constants/clothingType.js
+++ scripts/constants/clothingType.js
@@ -1,8 +1,8 @@
 const ClothingType = Object.freeze({
-    "hair" : "images/hair/hair.png",
+    "hair" : "images/hair/",
     "shirt" : "images/shirts/get hoodie.png",
     "pants" : "images/pants/get pants.png",
-    "shoes" : "images/shoes/get shoes.png",
-    "pet" : "images/pet/get dog.png",
+    "shoes" : "images/shoes/",
+    "pet" : "images/pets/get dog.png",
     "coin": "images/coin/get 3coins.png"
 });
